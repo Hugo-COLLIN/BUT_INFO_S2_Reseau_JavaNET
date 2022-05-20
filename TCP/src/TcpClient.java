881bd27d18e16e6db9b36e7c1141322d5947e1d9
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-05-20 12:08:05 +0200

    TCP fonctionnel sur 1re itération de boucle

--- TCP/src/TcpClient.java
+++ TCP/src/TcpClient.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class TcpClient {
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class TcpClient
+{
+    public static void main(String[] args) {
+        try
+        {
+            //Envoi des données
+            Socket socket = new Socket(args[0], Integer.parseInt(args[1]));
+            String strExchange = "";
+            Scanner sc = new Scanner(System.in);
+
+            PrintWriter toServer = new PrintWriter(
+                    new OutputStreamWriter(socket.getOutputStream())
+            );
+
+            BufferedReader fromServer = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
+
+            //boolean quit = strExchange.equals("stop".toLowerCase());
+            //quit = "stop".toLowerCase().equals(strExchange);
+            while (!strExchange.equals("stop"))
+            {
+                //Réception des données
+                strExchange = fromServer.readLine();
+                System.out.println("Reçu du serveur : " + strExchange);
+
+                //Envoi de données
+                if (!strExchange.equals("stop"))
+                {
+                    System.out.println("Entrer un message : ");
+                    strExchange = sc.nextLine();
+                    toServer.println(strExchange);
+                    toServer.flush();
+                }
+
+            }
+            toServer.close();
+            fromServer.close();
+            socket.close();
+
+        }
+        catch (SocketException e)
+        {
+            System.out.println("Erreur de socket");
+        }
+        catch (UnknownHostException e)
+        {
+            System.out.println("Hôte inconnu");
+        }
+        catch (IOException e)
+        {
+            System.out.println("Erreur d'entrée-sortie");
+            System.exit(1);
+        }
+
+    }
 }
--- TCP/src/TcpServer.java
+++ TCP/src/TcpServer.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class TcpServer {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class TcpServer
+{
+    public static void main(String[] args) {
+
+        try {
+            //Ouverture connexion passive
+            ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+            String strExchange = "";
+            Scanner sc = new Scanner(System.in);
+
+            while (!strExchange.equals("stop"))
+            {
+                System.out.println(strExchange);
+                System.out.println(strExchange.equals("stop".toLowerCase()));
+                //Connexion
+                Socket socket = serverSocket.accept();
+                BufferedReader fromClient = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream())
+                );
+
+                PrintWriter toClient = new PrintWriter(
+                        new OutputStreamWriter(socket.getOutputStream())
+                );
+
+                //Envoi de données
+                System.out.println("Entrer un message : ");
+                strExchange = sc.nextLine();
+                toClient.println(strExchange);
+                toClient.flush();
+
+                //Réception des données
+                if (!strExchange.equals("stop"))
+                {
+                    strExchange = fromClient.readLine();
+                    System.out.println("Reçu du client : " + strExchange);
+                }
+
+                toClient.close();
+                fromClient.close();
+                socket.close();
+            }
+            serverSocket.close();
+        }
+        catch (SocketException e)
+        {
+            System.out.println("Erreur de socket");
+        }
+        catch (UnknownHostException e)
+        {
+            System.out.println("Hôte inconnu");
+        }
+        catch (IOException e)
+        {
+            System.out.println("Erreur d'entrée-sortie");
+            System.exit(1);
+        }
+    }
 }
