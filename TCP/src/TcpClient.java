a025484d6a62df0b124301711649e5e0c4535c2e
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-05-22 23:19:06 +0200

    TCP entièrement fonctionnel

--- TCP/src/TcpClient.java
+++ TCP/src/TcpClient.java
@@ -9,10 +9,16 @@ public class TcpClient
     public static void main(String[] args) {
         try
         {
-            //Envoi des données
-            Socket socket = new Socket(args[0], Integer.parseInt(args[1]));
+            //Vérifie que le nombre d'arguments est correct
+            if (args.length != 2)
+                throw new ArrayIndexOutOfBoundsException();
+
             String strExchange = "";
             Scanner sc = new Scanner(System.in);
+
+            //Demande de connexion
+            Socket socket = new Socket(args[0], Integer.parseInt(args[1]));
+            System.out.println("Connecté au serveur " + socket.getInetAddress() + ":" + socket.getPort());
 
             PrintWriter toServer = new PrintWriter(
                     new OutputStreamWriter(socket.getOutputStream())
@@ -22,41 +28,55 @@ public class TcpClient
                     new InputStreamReader(socket.getInputStream())
             );
 
-            //boolean quit = strExchange.equals("stop".toLowerCase());
-            //quit = "stop".toLowerCase().equals(strExchange);
             while (!strExchange.equals("stop"))
             {
                 //Réception des données
                 strExchange = fromServer.readLine();
-                System.out.println("Reçu du serveur : " + strExchange);
+                System.out.println("\nReçu du serveur :\n" + strExchange);
 
                 //Envoi de données
                 if (!strExchange.equals("stop"))
                 {
-                    System.out.println("Entrer un message : ");
+                    System.out.println("\nEntrer un message : ");
                     strExchange = sc.nextLine();
                     toServer.println(strExchange);
                     toServer.flush();
                 }
-
             }
             toServer.close();
             fromServer.close();
             socket.close();
-
+            System.out.println("\nConnexion stoppée");
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("<!> Nombre d'arguments incorrect. Syntaxe : TcpClient [IPaddress] [portNumber]");
+            System.exit(1);
+        }
+        catch (NumberFormatException e)
+        {
+            System.out.println("<!> L'argument saisi n'est pas un numéro de port");
+            System.exit(2);
+        }
+        catch (IllegalArgumentException e)
+        {
+            System.out.println("<!> Le numéro de port doit être compris entre 0 et 65535");
+            System.exit(3);
         }
         catch (SocketException e)
         {
-            System.out.println("Erreur de socket");
+            System.out.println("<!> Erreur de socket");
+            System.exit(4);
         }
         catch (UnknownHostException e)
         {
-            System.out.println("Hôte inconnu");
+            System.out.println("<!> Hôte inconnu");
+            System.exit(5);
         }
         catch (IOException e)
         {
-            System.out.println("Erreur d'entrée-sortie");
-            System.exit(1);
+            System.out.println("<!> Erreur d'entrée-sortie");
+            System.exit(6);
         }
 
     }
--- TCP/src/TcpServer.java
+++ TCP/src/TcpServer.java
@@ -1,22 +1,27 @@
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.util.Scanner;
 
 public class TcpServer
 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
         try {
-            //Ouverture connexion passive
-            ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+            //Vérifie que le nombre d'arguments est correct
+            if (args.length != 1)
+                throw new ArrayIndexOutOfBoundsException();
+
             String strExchange = "";
             Scanner sc = new Scanner(System.in);
 
+            //Ouverture d'une connexion passive
+            ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+            System.out.println("Serveur lancé, en attente...");
+
             //Connexion
             Socket socket = serverSocket.accept();
+            System.out.println("Connecté au client " + socket.getInetAddress() + ":" + socket.getPort());
+
             BufferedReader fromClient = new BufferedReader(
                     new InputStreamReader(socket.getInputStream())
             );
@@ -27,13 +32,8 @@ public class TcpServer
 
             while (!strExchange.equals("stop"))
             {
-                //System.out.println(strExchange);
-                //System.out.println(strExchange.equals("stop".toLowerCase()));
-
-
-
                 //Envoi de données
-                System.out.println("Entrer un message : ");
+                System.out.println("\nEntrer un message : ");
                 strExchange = sc.nextLine();
                 toClient.println(strExchange);
                 toClient.flush();
@@ -42,27 +42,45 @@ public class TcpServer
                 if (!strExchange.equals("stop"))
                 {
                     strExchange = fromClient.readLine();
-                    System.out.println("Reçu du client : " + strExchange);
+                    System.out.println("\nReçu du client :\n" + strExchange);
                 }
-
-                toClient.close();
-                fromClient.close();
-                socket.close();
             }
+            toClient.close();
+            fromClient.close();
+            socket.close();
             serverSocket.close();
+            System.out.println("\nConnexion stoppée");
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("<!> Nombre d'arguments incorrect. Syntaxe : TcpServer [portNumber]");
+            System.exit(1);
+        }
+        catch (NumberFormatException e)
+        {
+            System.out.println("<!> L'argument saisi n'est pas un numéro de port");
+            System.exit(2);
+        }
+        catch (IllegalArgumentException e)
+        {
+            System.out.println("<!> Le numéro de port doit être compris entre 0 et 65535");
+            System.exit(3);
         }
         catch (SocketException e)
         {
-            System.out.println("Erreur de socket");
+            System.out.println("<!> Erreur de socket");
+            System.exit(4);
         }
         catch (UnknownHostException e)
         {
-            System.out.println("Hôte inconnu");
+            System.out.println("<!> Hôte inconnu");
+            System.exit(5);
         }
         catch (IOException e)
         {
-            System.out.println("Erreur d'entrée-sortie");
-            System.exit(1);
+            System.out.println("<!> Erreur d'entrée-sortie");
+            System.exit(6);
         }
+
     }
 }
